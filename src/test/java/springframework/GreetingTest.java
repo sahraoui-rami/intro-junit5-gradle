b97ade23212d7ce01d75cfa1e0251e07d70a4eec
ChefServiceInfo <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>
2023-11-17 11:25:49 +0100

    Setting up gradle dependencies and test configuration.

--- src/test/java/springframework/GreetingTest.java
+++ src/test/java/springframework/GreetingTest.java
@@ -1,52 +1,48 @@
 package springframework;
 
-//import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Rami SAHRAOUI on 17/11/2023
+ */
 class GreetingTest {
 
-    private Greeting greeting;
+    Greeting greeting;
 
-//    @BeforeAll
-//    public static void beforeClass(){
-//        System.out.println("Before - I am only called Once!!!");
-//    }
-//
-//    @BeforeEach
-//    void setUp() {
-//        System.out.println("In Before Each....");
-//
-//        greeting = new Greeting();
-//    }
-//
-//    @Test
-//    void helloWorld() {
-//        greeting = new Greeting();
-//
-//        System.out.println(greeting.helloWorld());
-//
-//    }
-//
-//    @Test
-//    void helloWorld1() {
-//        greeting = new Greeting();
-//
-//        System.out.println(greeting.helloWorld("John"));
-//    }
-//
-//    @Test
-//    void helloWorld2() {
-//        greeting = new Greeting();
-//
-//        System.out.println(greeting.helloWorld("Sam"));
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        System.out.println("In After Each........");
-//    }
-//
-//    @AfterAll
-//    public static void afterClass(){
-//        System.out.println("After!!! ***** - I am only called Once!!!");
-//    }
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("########################################");
+        System.out.println("Before - I am only called once!!!\n");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("\t*********************");
+        System.out.println("\tIn Before Each...");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println("\t\t" + greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println("\t\t" + greeting.helloWorld("Rami"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("\tIn After Each...");
+        System.out.println("\t*********************\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After - I am only called once!!!");
+        System.out.println("########################################\n\n");
+    }
 }
